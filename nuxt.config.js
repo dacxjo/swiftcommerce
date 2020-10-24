0724c4a5a8d8c6047c32a6b967ea54fbaf4d8775
David De Jesus <djesus1906.96@gmail.com>
2020-10-24 03:54:59 +0200

    Netlify cms setup complete

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,8 +22,9 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'author', href: '/humans.txt' }
+    ],
   },
   /*
   ** Global CSS
